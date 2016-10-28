080299d79b6dfa309c5ce0934467f156d6947fdd
Anurag Chitnis <anuragchitnis@my.unt.edu>
2016-10-28 17:29:55 -0500

    Removed the Navigation drawer as we don't need it anymore.

--- app/src/main/java/edu/unt/transportation/bustrackingsystem/MainActivity.java
+++ app/src/main/java/edu/unt/transportation/bustrackingsystem/MainActivity.java
@@ -2,11 +2,6 @@ package edu.unt.transportation.bustrackingsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -29,9 +24,7 @@ import java.util.ArrayList;
 
 import edu.unt.transportation.bustrackingsystem.model.BusStop;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener
-{
+public class MainActivity extends AppCompatActivity {
     public static final String ARG_ROUTE = "argRoute";
     public static final String ARG_PATHS = "argPaths";
     private static final String FIREBASE_URL = "https://untbustracking-acb72.firebaseio.com/";
@@ -44,57 +37,13 @@ public class MainActivity extends AppCompatActivity
             .build();
 
     @Override
-    public void onBackPressed()
-    {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer != null)
-        {
-            if (drawer.isDrawerOpen(GravityCompat.START))
-            {
-                drawer.closeDrawer(GravityCompat.START);
-            }
-            else
-            {
-                super.onBackPressed();
-            }
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null)
-        {
-            fab.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View view)
-                {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                    showMap();
-                }
-            });
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        if (drawer != null)
-        {
-            drawer.addDrawerListener(toggle);
-        }
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        if (navigationView != null)
-        {
-            navigationView.setNavigationItemSelectedListener(this);
-        }
         Firebase.setAndroidContext(this);
         final Firebase myFirebaseRef = new Firebase(FIREBASE_URL);
         myFirebaseRef.addAuthStateListener(new Firebase.AuthStateListener()
@@ -132,6 +81,8 @@ public class MainActivity extends AppCompatActivity
             }
         });
         Button firebaseTest = (Button) findViewById(R.id.firebase_test);
+        //Disabled the button to prevent it from making changes to database
+        firebaseTest.setEnabled(false);
         firebaseTest.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -191,47 +142,6 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item)
-    {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera)
-        {
-            // Handle the camera action
-        }
-        else if (id == R.id.nav_gallery)
-        {
-
-        }
-        else if (id == R.id.nav_slideshow)
-        {
-
-        }
-        else if (id == R.id.nav_manage)
-        {
-
-        }
-        else if (id == R.id.nav_share)
-        {
-
-        }
-        else if (id == R.id.nav_send)
-        {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer != null)
-        {
-            drawer.closeDrawer(GravityCompat.START);
-        }
-        return true;
     }
 
     private void showMap()
