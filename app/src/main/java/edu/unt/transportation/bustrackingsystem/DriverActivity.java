02ddaf1ad2acbc8de98f98e9875202836d70ce36
Satyanarayana <SatyanarayanChivukula@my.unt.edu>
2016-11-10 11:13:55 -0600

    DriverActivity.java partially commented

--- app/src/main/java/edu/unt/transportation/bustrackingsystem/DriverActivity.java
+++ app/src/main/java/edu/unt/transportation/bustrackingsystem/DriverActivity.java
@@ -1,6 +1,5 @@
 package edu.unt.transportation.bustrackingsystem;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -10,20 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -32,21 +24,43 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import edu.unt.transportation.bustrackingsystem.model.BusRoute;
 import edu.unt.transportation.bustrackingsystem.model.Vehicle;
 
+/**
+ * Created By: Anurag
+ * Revised By: Satyanarayana
+ * Descritpion: An activity that shows map and
+ * keep pushing the latest location to firebase
+ */
 public class DriverActivity extends AppCompatActivity implements OnMapReadyCallback,
         LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+    /**
+     * Firebase root URL
+     */
     private static final String FIREBASE_URL = "https://untbustracking-acb72.firebaseio.com/";
+
+    /**
+     * Firebase vehicle root, the vehicle that
+     * was selected by driver while logging in
+     */
     private static String FIREBASE_VEHICLE_NODE = null;
+
+    /**
+     * Firebase route root, the route that
+     * wast selected by driver while logging in
+     */
     private static String FIREBASE_ROUTE_NODE = null;
 
-    private static final int REQUEST_PLACE_PICKER = 1;
-
+    /**
+     * Google Map private variable, that was called
+     * onMapReady event fired
+     */
     private GoogleMap mMap;
+
+    /**
+     * Firebase instance variable
+     */
     private Firebase mFirebase;
     private GoogleApiClient mGoogleApiClient;
     private LatLngBounds.Builder mBounds = new LatLngBounds.Builder();
@@ -98,45 +112,6 @@ public class DriverActivity extends AppCompatActivity implements OnMapReadyCallb
 
     public void onDestroy() {
         super.onDestroy();
-    }
-
-    /**
-     * Prompt the user to check out of their location. Called when the "Check Out!" button
-     * is clicked.
-     */
-    public void checkOut(View view) {
-        try {
-            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
-            Intent intent = intentBuilder.build(this);
-            startActivityForResult(intent, REQUEST_PLACE_PICKER);
-        } catch (GooglePlayServicesRepairableException e) {
-            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
-                    REQUEST_PLACE_PICKER);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            Toast.makeText(this, "Please install Google Play Services!", Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_PLACE_PICKER) {
-            if (resultCode == Activity.RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-
-                Map<String, Object> checkoutData = new HashMap<>();
-                checkoutData.put("latitude", place.getLatLng().latitude);
-                checkoutData.put("longitude", place.getLatLng().longitude);
-
-                mFirebase.child(FIREBASE_VEHICLE_NODE).child(place.getId()).setValue(checkoutData);
-
-            } else if (resultCode == PlacePicker.RESULT_ERROR) {
-                Toast.makeText(this, "Places API failure! Check the API is enabled for your key",
-                        Toast.LENGTH_LONG).show();
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
     }
 
     /**
